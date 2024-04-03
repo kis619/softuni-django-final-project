0dc2b53dab6502b5c7abcd0284e8274eb5d282c8
Krissi <k.milcheva@turbit.de>
2024-04-03 10:27:27 +0200

    added password management (but no email managed pasword reset)

--- final_exam/accounts/urls.py
+++ final_exam/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 
 from final_exam.accounts.views import SignUpView, LutLoginView, LutLogoutView
@@ -6,5 +7,9 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LutLoginView.as_view(), name='login'),
     path('logout/', LutLogoutView.as_view(), name='logout'),
-
+    path('password_change/', PasswordChangeView.as_view(template_name='accounts/registration/password_change.html'),
+         name='password_change'),
+    path('password_change/done/',
+         PasswordChangeDoneView.as_view(template_name='accounts/registration/password_change_done.html'),
+         name='password_change_done'),
 ]
