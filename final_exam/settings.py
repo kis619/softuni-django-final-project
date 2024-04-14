d70e7fbe44f8810fe00bcdf25b3309029eb462f9
Krissi <k.milcheva@turbit.de>
2024-04-14 10:55:25 +0200

    trying things out

--- final_exam/settings.py
+++ final_exam/settings.py
@@ -128,13 +128,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'media'
+    os.path.join(BASE_DIR, 'media'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
