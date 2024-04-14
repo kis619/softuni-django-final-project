25fd2f5766d0cc34083a2cf4e8686f712c7dc71f
Krissi <k.milcheva@turbit.de>
2024-04-14 10:57:18 +0200

    renaming media back

--- final_exam/settings.py
+++ final_exam/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
